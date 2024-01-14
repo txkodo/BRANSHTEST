03e3ccc97825cb352ba58e34fe064a1b877ff960
BuildTools <unconfigured@null.spigotmc.org>
2024-01-15 01:44:30 +0900

    🚀

--- actions/build/update_version.js
+++ actions/build/update_version.js
@@ -1,10 +1,16 @@
-module.exports = async ({ version }) => {
+module.exports = async ({ release_type }) => {
   const fs = require('fs').promises;
   const content = JSON.parse(
     await fs.readFile('package.json', { encoding: 'utf8' })
   );
+
+  const [major, minor, patch] = content.version.split('.').map(parseInt);
+
   content.version = version;
+
   await fs.writeFile('package.json', JSON.stringify(content, undefined, 2), {
     encoding: 'utf8',
   });
+
+  return 
 };
